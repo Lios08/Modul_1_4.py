bf8c3b05e0768f232af2eacea6b49414dc14072f
Lios08 <afanaseevasd@icloud.com>
2024-11-14 16:53:47 +0500

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,9 @@
+first_name = 'Всего задач :'
+zadaniya = 12
+oclok = 'затрачено часов :'
+time = 1.5
+Curs = 'Курс: Python,'
+time_one_zadaniya = ( time / zadaniya )
+srednee = 'среднее время выполнения:'
+name = 'часа'
+print ( Curs, first_name, zadaniya, oclok, time, srednee, time_one_zadaniya, name)
